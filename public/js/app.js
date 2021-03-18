126c6f7ef0b807971c488515ef9326d03b9f4bdc
Yu <jeremy.yu@sap.com>
2021-03-18 12:31:41 -0700

    Remove unncessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // const fetch = require("node-fetch");
 
-console.log('Client side javascript file is loaded!')
+// console.log('Client side javascript file is loaded!')
 
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
